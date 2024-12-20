668b792c2534ef480273e31da98a608ae5e6ffbb
EdwardNew <pkarrows@gmail.com>
2024-12-20 04:05:26 -0800

    STABLE version; added fonts, updated some styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                body: ["Nunito", ...defaultTheme.fontFamily.sans],
+                heading: ["Amulya", ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    plugins: [],
+    darkMode: "selector",
+};
